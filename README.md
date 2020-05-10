20be49d872465fbc7b1f9a396632648e754921f1
Ekweaga <57598138+Ekweaga@users.noreply.github.com>
2020-05-11 00:24:23 +0100

    Update and rename README.md to copays.php

--- README.md
+++
@@ -1,2 +0,0 @@
-# acc-num
-how to generate random account number to user when they register a particular bank form
---
+++ copays.php
@@ -0,0 +1,54 @@
+<?php
+?>
+
+
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="description" content="Landing page of my website">
+        <meta http-quiv="refresh" content="3s">
+        <meta http-quiv="X-UA-Compatible" content="ie=edges">
+        <script src="jquery-3.4.1.min.js"></script>
+        <script src="copays.js"></script>
+        <script src="sweetalert.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+            </head>
+            <body>
+                <div class="header">
+                    <div class="header_img">
+                        <img scr="jj.jpg" alt="watch"></div>
+          <div><button class="signupbtn">Sign up</button></div>
+          <div><button>Sign in</button></div>         
+</div><br><div class="signup">
+<form action="" method="post">
+  <div class="imgcontainer">
+   <h1>
+    SIGN UP
+    </h1>
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Nationality</b></label>
+    <input type="text" placeholder="Enter Password" name="psw" required>
+    <label for="psw"><b>E-mail</b></label>
+    <input type="text" placeholder="Enter Password" name="psw" required>
+    <label for="psw"><b>Address</b></label>
+    <input type="text" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+  </div>
+</form></div>
+</body>
